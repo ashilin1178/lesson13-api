3bbf91fb2958632741fe879c51aa2b1ea2b22c4c
Gleb Kushedow <kushedow@gmail.com>
2022-08-03 18:22:21 +0300

    run is now in app start in now outside app conftest in now in root imports updated

--- conftest.py
+++ conftest.py
@@ -1,10 +1,9 @@
 import pytest
 
-import run
+from app.run import app
 
 
 # создаем фикстуру для тестирования всех вьюшек (main, candidates, vacancies)
 @pytest.fixture()
 def test_client():
-    app = run.app
     return app.test_client()
